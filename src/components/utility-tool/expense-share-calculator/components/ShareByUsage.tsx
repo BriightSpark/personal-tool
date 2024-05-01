3c452cfd8fba46fced85a175d2270569ae2af02f
Billy Tran <billy.tran186@gmail.com>
2024-05-01 10:40:08 +1000

    feat: using date picker

--- src/components/utility-tool/expense-share-calculator/components/ShareByUsage.tsx
+++ src/components/utility-tool/expense-share-calculator/components/ShareByUsage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import cn from 'classnames';
 
 import { Expense, ExpenseSplit, User } from '../type';
+import { Button } from '@briightspark/ui-components';
 
 type ShareByUsageType = {
   users: User[]
@@ -23,6 +26,7 @@ export const ShareByUsage = ( {
   const [description, setDescription] = useState<string>( initDescription );
   const [cost, setCost] = useState<number>( expense.amount );
   const [usage, setUsage] = useState<number[]>( new Array( users.length ).fill( 0 ) );
+  const [selectedDates, setSelectedDates] = useState<Date[][]>( [] );
 
   useEffect( () => {
     const totalUsage = usage.reduce( ( s, c ) => s + c, 0 );
@@ -58,6 +62,20 @@ export const ShareByUsage = ( {
     onExpenseUpdate( { ...expense, amount: c } );
   };
 
+  const handleDateChange = ( { d, idx } : { d : Date[] | null, idx: number } ) => {
+    setSelectedDates( [
+      ...selectedDates.slice( 0, idx ),
+      d || [],
+      ...selectedDates.slice( idx + 1 )
+    ] );
+
+    setUsage( [
+      ...usage.slice( 0, idx ),
+      d ? d.length : 0,
+      ...usage.slice( idx + 1 )
+    ] );
+  };
+
   return (
     <tr className='expense-calculator__table-body-content'>
       <td className='expense-calculator__table-body-content-description'>
@@ -74,16 +92,14 @@ export const ShareByUsage = ( {
       </td>
       {
         users.map( ( u, idx ) => (
-          <td key={ u.id } className='expense-calculator__table-body-content-value'>
-            <input
-              type='number'
-              defaultValue={ usage[idx] }
-              placeholder={ String( usage[idx] ) }
-              onChange={ ( e ) => setUsage( [
-                ...usage.slice( 0, idx ),
-                Number( e.target.value ),
-                ...usage.slice( idx + 1 )
-                ] ) }
+          <td key={ u.id } className='expense-calculator__table-body-content-value overflow-visible'>
+            <DatePicker
+              selectsMultiple
+              selectedDates={ selectedDates[idx] }
+              customInput={ <Button>{ usage[idx] } day(s)</Button> }
+              onChange={ ( d ) => handleDateChange( { d, idx } ) }
+              shouldCloseOnSelect={ false }
+              disabledKeyboardNavigation
             />
           </td>
         ) )
